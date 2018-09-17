be026e79079d66b8df2e1d04257980875bf3f8d7
Raül Pérez <repejota@gmail.com>
2018-09-18 00:37:08 +0200

    Fixed package docs

--- doc.go
+++ doc.go
@@ -21,14 +21,14 @@ type. A set is an abstract data type that can store unique values.
 
 The curent implementation:
 
-  - Is a dynamic set.
-    It implements actions to Add() or Remove() elements from it among others.
-  - Is ordered.
-    Instead the generic set, this implementation is insertion ordered,
-    meaning that when iterating over its elements, it will return them in
-	order they where inserted originally.
+	* Is a dynamic set.
+	  It implements actions to Add() or Remove() elements from it among others.
+	* Is ordered.
+	  Instead the generic set, this implementation is insertion ordered,
+	  meaning that when iterating over its elements, it will return them in
+	  order they where inserted originally.
 
-Example
+Examples:
 
 	package main
 
